f9f4e057928f43db54c1a5b269226ca38b11b6b1
vinayhs-7865 <vinayhs7865@gmail.com>
2025-01-24 16:20:46 +0530

    js file modified

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-let userName = prompt("Please enter access userName");
-if (userName=="vinay"){
-    alert("Welcome to Portfolio",userName);
-}
-else{
-    alert("please enter correct acess userName")
-    
-}
-userName=prompt("Please enter correct access userName");
-      
+
